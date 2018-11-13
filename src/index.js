447f4321e9b35434dad1c722a98725178a19cc80
Raymond Huynh <raymond.h@myplanet.com>
2018-11-12 23:48:57 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,104 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import styled from 'styled-components';
+import { DragDropContext } from 'react-beautiful-dnd';
+import '@atlaskit/css-reset';
+import initialData from './initial-data';
+import Column from './Column';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialData;
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result) {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.draggableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+  
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+  
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn,
+        },
+      }
+      this.setState(newState);
+    } else {
+      const sourceTaskIds = Array.from(start.taskIds);
+      sourceTaskIds.splice(source.index, 1);
+      const newStartColumn = {
+        ...start,
+        taskIds: sourceTaskIds,
+      };
+
+      const newSourceTaskIds = Array.from(finish.taskIds);
+      newSourceTaskIds.splice(destination.index, 0, draggableId);
+      const newEndColumn = {
+        ...finish,
+        taskIds: newSourceTaskIds,
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newStartColumn.id]: newStartColumn,
+          [newEndColumn.id]: newEndColumn,
+        }
+      }
+      this.setState(newState);
+    }
+  }
+
+  render() {
+    return (
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}
+      >
+        <Container>
+          {
+            this.state.columnOrder.map(columnId => {
+              const column = this.state.columns[columnId];
+              const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+              
+              return <Column key={column.id} column={column} tasks={tasks} />;
+            })
+          }
+        </Container>
+      </DragDropContext>
+    );
+  }
+} 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
